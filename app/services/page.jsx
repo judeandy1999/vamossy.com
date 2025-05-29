a9f8c50ef3c484ada6b58bd2de38231bc5e1f55e
jconvencido <jconvencido@fullscale.io>
2025-05-29 22:33:56 +0800

    Refactor Page component to include additional sections and improve layout

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,8 +1,26 @@
 import ServicesHero from '@/components/services-hero';
-import { services } from '../../data/services';
+import NeedHelp from '@/components/ui/need-help';
+import Footer from '@/components/footer';
+import AwardsSection from '@/components/awards-section';
+import DigitalMarketingServices from '@/components/digital-marketing-services';
+import ServicesPromoSection from '@/components/services-promo-section';
+import { promoSections } from "@/data/data";
+import TrustedBySection from '@/components/trustedby-section';
 
 export default function Page() {
+  
   return (
-    <ServicesHero />
+    <div className='pt-12'>
+      <NeedHelp/>
+      <ServicesHero />
+      <AwardsSection />
+      <DigitalMarketingServices />
+      {promoSections.map((section, index) => (
+        <ServicesPromoSection key={index} {...section} />
+      ))}
+      <TrustedBySection />
+
+      <Footer />
+    </div>
   );
 }
