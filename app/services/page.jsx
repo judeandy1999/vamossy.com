bdf7b03a96989c63ad5877583f1a02dd3ea34334
jconvencido <jconvencido@fullscale.io>
2025-05-31 22:10:39 +0800

    fix: update import paths for services components in page.jsx

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,11 +1,11 @@
-import ServicesHero from '@/components/services-hero';
+import ServicesHero from '@/components/services/services-hero';
 import NeedHelp from '@/components/ui/need-help';
-import Footer from '@/components/footer';
-import AwardsSection from '@/components/awards-section';
-import DigitalMarketingServices from '@/components/digital-marketing-services';
-import ServicesPromoSection from '@/components/services-promo-section';
+import Footer from '@/components/ui/footer';
+import AwardsSection from '@/components/services/awards-section';
+import DigitalMarketingServices from '@/components/services/digital-marketing-services';
+import ServicesPromoSection from '@/components/services/services-promo-section';
 import { promoSections } from "@/data/data";
-import TrustedBySection from '@/components/trustedby-section';
+import TrustedBySection from '@/components/services/trustedby-section';
 
 export default function Page() {
   
