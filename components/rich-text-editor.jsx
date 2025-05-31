c11cc1a842ce9ee0797c43015a427859d185baa2
jconvencido <jconvencido@fullscale.io>
2025-05-31 22:11:15 +0800

    feat: implement RichTextEditor component with customizable text formatting options

---
+++ components/rich-text-editor.jsx
@@ -0,0 +1,64 @@
+// components/RichTextEditor.jsx
+'use client';
+
+import { useEditor, EditorContent } from '@tiptap/react';
+import { useEffect, useState } from 'react';
+import StarterKit from '@tiptap/starter-kit';
+import Table from '@tiptap/extension-table';
+import TableRow from '@tiptap/extension-table-row';
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+import Link from '@tiptap/extension-link';
+import TextStyle from '@tiptap/extension-text-style';
+import EditorToolbar from './editor-toolbar';
+import Underline from '@tiptap/extension-underline';
+import TextAlign from '@tiptap/extension-text-align';
+
+export default function RichTextEditor({ content, onContentChange }) {
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Table.configure({ resizable: true }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Link.configure({ openOnClick: false }),
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+      }),
+      TextStyle.extend({
+        addAttributes() {
+          return {
+            fontSize: {
+              default: null,
+              parseHTML: element => element.style.fontSize || null,
+              renderHTML: attributes => {
+                if (!attributes.fontSize) return {};
+                return { style: `font-size: ${attributes.fontSize}` };
+              },
+            },
+          };
+        },
+      }),
+    ],
+    content: content || '',
+    onUpdate: ({ editor }) => {
+      const html = editor.getHTML();
+      onContentChange(html);
+    },
+  });
+
+  return (
+    <div className='relative'>
+      <div className='absolute right-0 -top-0 w-full'>
+        <EditorToolbar editor={editor} />
+      </div>
+      <EditorContent
+        editor={editor}
+        className="border z-5 pt-13 p-4 min-h-[400px] rounded bg-white shadow focus:outline-none flex flex-col"
+      />
+    </div>
+  );
+};
