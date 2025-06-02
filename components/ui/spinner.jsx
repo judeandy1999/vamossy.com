1a4ee3a598829a8ed019f426cdbd4a3d4e4f1df2
jconvencido <jconvencido@fullscale.io>
2025-06-02 21:51:40 +0800

    feat: add spinner component for loading indication

---
+++ components/ui/spinner.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Spinner() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-white">
+      <div className="w-12 h-12 border-4 border-blue-400 border-dashed rounded-full animate-spin"></div>
+    </div>
+  );
+}
