104881d5407857bd3e207e92bb01594f418e58dc
jconvencido <jconvencido@fullscale.io>
2025-06-03 23:07:05 +0800

    fix: update RichTextEditor props to include contentChanged for better content management

--- components/rich-text-editor.jsx
+++ components/rich-text-editor.jsx
@@ -13,7 +13,7 @@ import EditorToolbar from './editor-toolbar';
 import Underline from '@tiptap/extension-underline';
 import TextAlign from '@tiptap/extension-text-align';
 
-export default function RichTextEditor({ selectedAritcle, content, onContentChange }) {
+export default function RichTextEditor({ contentChanged, selectedAritcle, content, onContentChange }) {
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -52,7 +52,7 @@ export default function RichTextEditor({ selectedAritcle, content, onContentChan
     if (editor) {
       editor.commands.setContent(content);
     }
-  }, [selectedAritcle, editor]);  
+  }, [contentChanged,selectedAritcle, editor]);
 
   return (
     <div className='relative'>
