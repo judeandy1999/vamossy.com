9b10275beef83186a84378c39c075ac448462a50
jconvencido <jconvencido@fullscale.io>
2025-06-05 17:00:21 +0800

    refactor: ensure consistent return statement in GET request handler

--- pages/api/articles/[id].js
+++ pages/api/articles/[id].js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
         }, {});
       }
 
-      res.status(200).json(article);
+      return res.status(200).json(article);
     } catch (error) {
       console.error(error.message);
       res.status(500).json({ error: error.message });
