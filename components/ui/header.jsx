0a9090333bfab75434ae7988663c1d8fc744a594
jconvencido <jconvencido@fullscale.io>
2025-06-08 01:33:21 +0800

    refactor: update Header component to use useAuthWithRedirect and enhance sign-out functionality

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -4,12 +4,13 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation';
 import { navItems } from '@/data/data';
-import { signOut, useSession } from 'next-auth/react';
+import { useAuthWithRedirect } from '@/hooks/useAuthWithRedirect';
+import { signOut } from '@/utils/authService';
 
 export default function Header() {
+  const { status, session, role } = useAuthWithRedirect();
   const [isScrolled, setIsScrolled] = useState(false)
   const [isClient, setIsClient] = useState(false);
-  const { data: session } = useSession();
   const pathname = usePathname();
 
   useEffect(() => {
@@ -39,7 +40,7 @@ export default function Header() {
         </Link>
 
         <ul className="flex space-x-8 font-medium">
-          {(isClient && !(pathname === '/create-article' || pathname === '/options')) && navItems.map((item) => (
+          {(isClient && !(pathname === '/create-article' || pathname === '/options' || pathname === '/user-dashboard')) && navItems.map((item) => (
               <Link
                 key={item.name}
                 href={item.href}
@@ -51,7 +52,7 @@ export default function Header() {
               </Link>
             ))}
 
-          {session && (
+          {(session && role === 'admin') && (
             <>
               <Link
                 key={'create-article'}
@@ -71,13 +72,23 @@ export default function Header() {
                 }>
                 Options
               </Link>
-              <button
-                onClick={() => signOut()}
-                className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition-colors"
-              >
-                Log out
-              </button>
             </>
+          )}
+
+          {session && (
+            <button
+              onClick={async () => {
+                const { error } = await signOut();
+                if (error) {
+                  console.error('Error signing out:', error.message);
+                } else {
+                  window.location.href = '/login';
+                }
+              }}
+              className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition-colors"
+            >
+              Log out
+            </button>
           )}
         </ul>
       </nav>
