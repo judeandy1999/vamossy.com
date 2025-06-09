c7b5f8286386a2582740e9121d9f25139437c1b6
jconvencido <jconvencido@fullscale.io>
2025-06-10 00:21:39 +0800

    feat: fetch user data and send to HubSpot on home page load

--- app/page.js
+++ app/page.js
@@ -6,8 +6,23 @@ import OurStory from "@/components/homepage/our-story";
 import RemarkableResults from "@/components/homepage/remarkable-results";
 import TeamSection from "@/components/homepage/team-section";
 import Footer from "@/components/ui/footer";
+import { getUser } from '@/utils/authService';
+import { useSendToHubSpot } from '@/hooks/useSendToHubSpot';
 
 export default function Home() {
+    const { sendToHubSpot } = useSendToHubSpot();
+
+    useEffect(() => {
+      const fetchUser = async () => {
+        const { user } = await getUser();
+        if (user) {
+          await sendToHubSpot(user);
+        }
+      };
+    
+      fetchUser();
+    }, []);
+
     return (
       <>
         <NeedHelp />
