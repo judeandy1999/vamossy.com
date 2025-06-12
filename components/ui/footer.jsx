4541e39886cfed981c86eb49ecfa5a31c8d4d161
jconvencido <jconvencido@fullscale.io>
2025-06-13 00:25:05 +0800

    feat: update Footer component styles for improved readability and layout

--- components/ui/footer.jsx
+++ components/ui/footer.jsx
@@ -4,11 +4,11 @@ import { footerLinks } from "@/data/data";
 export default function Footer() {
   return (
     <>
-      <footer className="bg-[#0a1e2d] text-white py-12">
+      <footer className="bg-[#333] text-white py-4 md:py-6">
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-6">
           {footerLinks.map((section, index) => (
             <div key={index}>
-              <h3 className="text-lg font-semibold mb-4">{section.title}</h3>
+              <h3 className="text-md md:text-lg font-semibold mb-2">{section.title}</h3>
               {section.title === "Recent Blog Posts" ? (
                 <ul className="space-y-4">
                   {section.items.map((post, idx) => (
@@ -16,7 +16,7 @@ export default function Footer() {
                       <p className="text-sm text-gray-400">{post.date}</p>
                       <a
                         href="#"
-                        className="text-white hover:text-yellow-400 transition"
+                        className="text-sm md:text-md text-white hover:text-yellow-400 transition"
                       >
                         {post.title}
                       </a>
@@ -26,10 +26,10 @@ export default function Footer() {
               ) : (
                 <ul className="space-y-2">
                   {section.items.map((item, idx) => (
-                    <li key={idx}>
+                    <li className="mb-2" key={idx}>
                       <a
                         href="#"
-                        className="text-gray-400 hover:text-yellow-400 transition"
+                        className="text-sm md:text-md text-gray-400 hover:text-yellow-400 transition"
                       >
                         {item}
                       </a>
