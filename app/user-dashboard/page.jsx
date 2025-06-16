77cfc75a212f30cce53cfa7a9b72d0544b41082d
jconvencido <jconvencido@fullscale.io>
2025-06-17 01:20:59 +0800

    feat: implement user fetching and dashboard layout

--- app/user-dashboard/page.jsx
+++ app/user-dashboard/page.jsx
@@ -1,7 +1,31 @@
+
+'use client';
+import { useEffect } from "react";
+import { getUser } from '@/utils/authService';
+import { useSendToKlaviyo } from '@/hooks/useSendToKlaviyo';
+
 export default function UserDashboard() {
+  const { sendToKlaviyo } = useSendToKlaviyo();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { user } = await getUser();
+      if (user) {
+        await sendToKlaviyo(user);
+      }
+    };
+  
+    fetchUser();
+  }, []);
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '24px' }}>
-      <p>Role not admin</p>
+    <div className="p-6">
+      <div className="bg-white shadow rounded-lg p-6">
+        <h1 className="text-2xl font-semibold text-slate-800 mb-4">Welcome to Dashboard</h1>
+        <p className="text-gray-600">
+          Select an option from the sidebar to get started.
+        </p>
+      </div>
     </div>
   );
 }
