5ea87c5758261c9a6ff6c34f266d1a7395670979
ajnconvencido <convencidoarjoncarpio@gmail.com>
2025-06-22 01:53:45 +0800

    consulting update

--- components/consulting/consulting-hero.jsx
+++ components/consulting/consulting-hero.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { motion } from 'framer-motion';
+import Title from '@/components/ui/title';  
 
 export default function ConsultingHero() {
   const h1Variants = {
@@ -48,28 +49,12 @@ export default function ConsultingHero() {
         initial="hidden"
         animate="visible"
       >
-        <motion.h1
-          className="text-xl md:text-5xl lg:text-6xl font-semibold md:max-w-[80%] leading-tight"
-          variants={h1Variants}
-        >
-          Strategic Intelligence &amp; Growth Enablement for AI-Driven Brands
-        </motion.h1>
-        
-        <motion.h4
-          className="lg:mt-8 text-gray-200 text-md md:text-xl lg:text-2xl mt-4 font-light md:max-w-[60%] leading-tight"
-          variants={subheadingVariants}
-        >
-          We specialize in diagnosing complex business challenges and delivering high-performance solutions at the intersection of AI, ecommerce, and digital strategy.
-        </motion.h4>
-        <motion.p
-          className="lg:mt-4 text-gray-200 text-md md:text-xl lg:text-2xl mt-4 font-light md:max-w-[60%] leading-tight"
-          variants={paragraphVariants}
-        >
-          Our consulting division is designed for brands seeking to evolve intelligently — not just scale blindly.<br />
-          <span className="font-semibold text-yellow-400">We don't offer generic playbooks. We engineer outcomes.</span>
-        </motion.p>
+        <Title title="Strategic Intelligence &amp; Growth Enablement for AI-Driven Brands" />
+        <Title variant="h3" title="We specialize in diagnosing complex business challenges and delivering high-performance solutions at the intersection of AI, ecommerce, and digital strategy. Complexity into Clarity" />
+        <Title variant="h4" title="Our consulting division is designed for brands seeking to evolve intelligently — not just scale blindly." />
+        <Title variant="h4" title="We don't offer generic playbooks. We engineer outcomes." />
+   
       </motion.div>
-      <div className="absolute bottom-0 left-0 w-full h-52 bg-gradient-to-b from-transparent to-[#262626] z-10"></div>
     </section>
   );
 }
